94e79f580b741d17c738cd8aa217b68954f264dc
Christoph Burschka <christoph@burschka.de>
2016-10-31 09:57:46 +0100

    Demonstrate improperly nested tags.

--- sample.js
+++ sample.js
@@ -2,9 +2,10 @@ const XBBCode = require('./src/xbbcode.js');
 
 const filter = XBBCode.create({
   b: '<strong>{content}</strong>',
+  i: '<em>{content}</em>',
   url: tag => `<a href="${tag.getOption()}">${tag.getContent()}</a>`,
 });
 
-const input = 'Hello [b][url=https://github.com/cburschka/xbbcode.js]world[/url][/b].';
+const input = 'Hello [b][url=https://github.com/cburschka/xbbcode.js]world[/url][/b]. This [i]Tag [b][/i] is broken. [/b]';
 console.log(">>>", input);
 console.log(filter(input));
